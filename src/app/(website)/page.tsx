6e78458431c49dceb38afb2ea73f5cae835ab7c3
Kongkon-79 <kongkonjowarder79@gmail.com>
2025-06-14 18:55:34 +0600

    problem fix up

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+// import HomePageAllComponents from "./_components/HomePageAllComponents/HomePageAllComponents";
 
-
-import VideosPage from './videos/page';
+// import VideosPage from './videos/page';
 
 const HomePage = () => {
   return (
     <div className="">
-      <section>
+      <section className="h-screen w-full flex items-center justify-center">
+        {/* <HomePageAllComponents /> */}
+        <h2 className="text-3xl font-bold text-black leading-noraml">
+          Home Page No Complete
+        </h2>
       </section>
-      <VideosPage/>
+      {/* <VideosPage/> */}
     </div>
   );
 };
