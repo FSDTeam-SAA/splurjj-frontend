169c839233732ca073cf2e8068fcb8a0eb6ccf72
Kongkon-79 <kongkonjowarder79@gmail.com>
2025-07-27 16:56:25 +0600

    fix up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,16 @@ const config: Config = {
   			secondary: {
   				'50': '#E6EEFE',
   				DEFAULT: '#424242'
+  			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
   			}
   		},
   		keyframes: {
